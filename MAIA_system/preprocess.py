da17301b3f4249f0ceb8890046e5c519eff0383e
Maghav <mkumar10@illinois.edu>
2017-07-18 15:09:39 -0500

    Hierarchy file

--- MAIA_system/preprocess.py
+++ MAIA_system/preprocess.py
@@ -1,14 +1,14 @@
 import os
 import numpy as np
 import sklearn
-from sklearn.decompostion import PCA
-arr = #assume the array is obtained
-no_components = 2
+from sklearn.naive_bayes import GaussianNB
+from modis_dataset_gen import modis_dataset_generator
+#arr = #assume the array is obtained
+modis_dataset_generator("MOD021KM.A2004026.1230.006.2014218105922.hdf","output1.npz","MOD03.A2004026.1230.006.2012274025923.hdf")
+curr_data = np.load("output1.npz")
 
-def perform_pca(training_data):
+curr_image = curr_data["arr_0"]
 
-    pca = PCA(n_components=no_components)
-    pca.fit(training_data)
-    pca.transform(training_data)
+#no_components = 2
 
-    return training_data
+print(curr_image.shape)
